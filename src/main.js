1996d55f0b8faefdd3bf073ee07b1d9ad4942a92
Kateryna <koliesnikova.kateryna@gmail.com>
2024-08-25 22:34:01 +0300

    1

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const isSuccess = true;
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (isSuccess) {
+      resolve("Success! Value passed to resolve function");
+    } else {
+      reject("Error! Error passed to reject function");
+    }
+  }, 2000);
+});
+
+console.log(promise); // Об'єкт промісу
